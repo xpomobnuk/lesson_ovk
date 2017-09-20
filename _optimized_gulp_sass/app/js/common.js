ac7fc4737b8ecfdb9be1b64ac9255699dda55ba2
Vladimir <xpomobnuk@gmail.com>
2017-09-20 17:40:38 +0300

    new pc

--- _optimized_gulp_sass/app/js/common.js
+++ _optimized_gulp_sass/app/js/common.js
@@ -1,16 +1,69 @@
 $(function() {
 
+
+
+
 	$(".mfp-gallery").magnificPopup({
+		mainClass: 'mfp-zoom-in',
 		type: 'image',
-		closeOnContentClick: true,
-		image: {
-			verticalFit: false
+		tLoading: '',
+		gallery:{
+			enabled:true,
 		},
-		gallery: {
-			enabled: true
+		removalDelay: 300,
+		callbacks: {
+			beforeChange: function() {
+				this.items[0].src = this.items[0].src + '?=' + Math.random(); 
+			},
+			open: function() {
+				$.magnificPopup.instance.next = function() {
+					var self = this;
+					self.wrap.removeClass('mfp-image-loaded');
+					setTimeout(function() { $.magnificPopup.proto.next.call(self); }, 120);
+				}
+				$.magnificPopup.instance.prev = function() {
+					var self = this;
+					self.wrap.removeClass('mfp-image-loaded');
+					setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
+				}
+			},
+			imageLoadComplete: function() { 
+				var self = this;
+				setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
+			}
 		}
 	});
 
+	$(".mfp-certificate").magnificPopup({
+		mainClass: 'mfp-zoom-in',
+		type: 'image',
+		tLoading: '',
+		gallery:{
+			enabled:true,
+		},
+		removalDelay: 300,
+		callbacks: {
+			beforeChange: function() {
+				this.items[0].src = this.items[0].src + '?=' + Math.random(); 
+			},
+			open: function() {
+				$.magnificPopup.instance.next = function() {
+					var self = this;
+					self.wrap.removeClass('mfp-image-loaded');
+					setTimeout(function() { $.magnificPopup.proto.next.call(self); }, 120);
+				}
+				$.magnificPopup.instance.prev = function() {
+					var self = this;
+					self.wrap.removeClass('mfp-image-loaded');
+					setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
+				}
+			},
+			imageLoadComplete: function() { 
+				var self = this;
+				setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
+			}
+		}
+	});
 
 
 	$(".mouse-icon").click(function() {
@@ -19,11 +72,36 @@ $(function() {
 		}, 800);
 	});
 
+	$(".s-adv").waypoint(function() {
+		$({blurRadius: 5}).animate({blurRadius: 0}, {
+			duration: 1200,
+			easing: 'swing',
+			step: function() {
+				$(".s-adv-item h3 span").css({
+					"-webkit-filter": "blur("+this.blurRadius+"px)",
+					"filter": "blur("+this.blurRadius+"px)"
+				});
+			}
+		});
+		var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(' ');
+		$(".s-adv-item h3 span").each(function() {
+			var tcount = $(this).data("count");
+			$(this).animateNumber({ number: tcount,
+				easing: 'easeInQuad',
+				"font-size": "1.8125em",
+				numberStep: comma_separator_number_step},
+				1200);
+		});
+	}, {
+		offset: '80%'
+	});
+
+
 
 	$(".toggle-mnu").click(function() {
-	  $(this).toggleClass("on");
-	  $(".main-mnu").slideToggle();
-	  return false;
+		$(this).toggleClass("on");
+		$(".main-mnu").slideToggle();
+		return false;
 	});
 
 
@@ -63,5 +141,41 @@ $(function() {
 	};
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+	// Replace all SVG images with inline SVG
+
+	$('img.img-svg').each(function(){
+		var $img = $(this);
+		var imgID = $img.attr('id');
+		var imgClass = $img.attr('class');
+		var imgURL = $img.attr('src');
+
+		$.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = $(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+        	$svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+        	$svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+        	$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+        }
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+      }, 'xml');
+
+	});
 
 });
